1a43378637bc54393efe411d9bce97ddaa1bfbfb
Tirta <tirta@gmail.com>
2017-11-09 08:24:25 -0200

    scripts de criacao, sem revisao

--- user.sql
+++ user.sql
@@ -1,4 +1,4 @@
-CREATE TABLE User (
+CREATE TABLE user (
     user_id INT NOT NULL AUTO INCREMENT,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE User (
     id_schedule INT NOT NULL,
     id_notification INT NOT NULL,
     PRIMARY KEY (user_id),
-    FOREIGN KEY (id_schedule) REFERENCES Schedule(schedule_id),
-    FOREIGN KEY (id_notification) REFERENCES Notification(notification_id)
+    FOREIGN KEY (id_schedule) REFERENCES schedule(schedule_id),
+    FOREIGN KEY (id_notification) REFERENCES notification(notification_id)
 );
 
