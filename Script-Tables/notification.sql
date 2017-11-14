0b12c238dfabda8b096ba9556a272b3e0b32131a
guizg <guizgraicer@gmail.com>
2017-11-14 07:53:12 -0200

    fredado da arquitetura feita do banco

--- Script-Tables/notification.sql
+++ Script-Tables/notification.sql
@@ -1,9 +1,8 @@
 CREATE TABLE notification(
     id int not null;
-    title varchar(100) not null; --Para mostrar o historico de notificacoes
+    note varchar(100) not null; --Para mostrar o historico de notificacoes
     status boolean not null;
     id_group int(10)
     FOREIGN KEY (id_group) REFERENCES group(id),
-    FOREIGN KEY (title) REFERENCES group(title), --o titulo do historico eh o nome do grupo, para poder automatizar
     PRIMARY KEY (id)
 )
--- Script-Tables/user.sql
+++ Script-Tables/user.sql
@@ -1,10 +1,8 @@
 CREATE TABLE User (
-    user_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(20) NOT NULL,
-    id_schedule INT NOT NULL,
-    id_notification INT NOT NULL,
-    PRIMARY KEY (user_id)
+    PRIMARY KEY (id)
 );
 
