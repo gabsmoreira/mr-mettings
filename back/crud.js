403e7fbfc377236a4b0bed082409e7a0ea2b19d1
Tirta <tirta@gmail.com>
2017-11-22 08:11:20 -0200

    delete schedule

--- back/crud.js
+++ back/crud.js
@@ -18,7 +18,7 @@ app.listen(3001, function () {
 var connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : '160520',
+    password : '201096',
     database : 'mrmeetings'
     })
 
@@ -237,4 +237,85 @@ app.post('/findSchedule', function (req, res){
 
     });
 
+});
+
+
+
+//Update Schedule para deletar horario(seta 1 para os horarios quando deleta, fica disponivel)
+app.post('/deleteSchedule', function (req, res){
+    var start = req.body.start;
+    var end = req.body.end;
+    var day = req.body.day;
+    var id = req.body.id;
+
+    days = [["monday_6","monday_7","monday_8","monday_9","monday_10","monday_11","monday_12","monday_13","monday_14","monday_15","monday_16","monday_17","monday_18","monday_19","monday_20","monday_21","monday_22","monday_23"],
+    ["tuesday_6","tuesday_7","tuesday_8","tuesday_9","tuesday_10","tuesday_11","tuesday_12","tuesday_13","tuesday_14","tuesday_15","tuesday_16","tuesday_17","tuesday_18","tuesday_19","tuesday_20","tuesday_21","tuesday_22","tuesday_23"],
+    ["wednesday_6","wednesday_7","wednesday_8","wednesday_9","wednesday_10","wednesday_11","wednesday_12","wednesday_13","wednesday_14","wednesday_15","wednesday_16","wednesday_17","wednesday_18","wednesday_19","wednesday_20","wednesday_21","wednesday_22","wednesday_23"],
+    ["thursday_6","thursday_7","thursday_8","thursday_9","thursday_10","thursday_11","thursday_12","thursday_13","thursday_14","thursday_15","thursday_16","thursday_17","thursday_18","thursday_19","thursday_20","thursday_21","thursday_22","thursday_23"],
+    ["friday_6","friday_7","friday_8","friday_9","friday_10","friday_11","friday_12","friday_13","friday_14","friday_15","friday_16","friday_17","friday_18","friday_19","friday_20","friday_21","friday_22","friday_23"],
+    ["saturday_6","saturday_7","saturday_8","saturday_9","saturday_10","saturday_11","saturday_12","saturday_13","saturday_14","saturday_15","saturday_16","saturday_17","saturday_18","saturday_19","saturday_20","saturday_21","saturday_22","saturday_23"],
+    ["sunday_6","sunday_7","sunday_8","sunday_9","sunday_10","sunday_11","sunday_12","sunday_13","sunday_14","sunday_15","sunday_16","sunday_17","sunday_18","sunday_19","sunday_20","sunday_21","sunday_22","sunday_23"]  
+    ]
+
+    switch(day){
+        case 0:
+            for (i = start; i < end; i++){
+                qry = "UPDATE Schedule SET "+days[0][i-6]+"=? WHERE id_user=?";
+                connection.query(qry,[1, id],  function (error, results, fields) {
+                    if (error) throw error;
+                });
+            }
+            break;
+        case 1:
+            for (i = start; i < end; i++){
+                qry = "UPDATE Schedule SET "+days[1][i-6]+"=? WHERE id_user=?";
+                connection.query(qry,[1, id],  function (error, results, fields) {
+                    if (error) throw error;
+                });
+            }
+            break;
+        case 2:
+            for (i = start; i < end; i++){
+                qry = "UPDATE Schedule SET "+days[2][i-6]+"=? WHERE id_user=?";
+                connection.query(qry,[1, id],  function (error, results, fields) {
+                    if (error) throw error;
+                });
+            }
+            break;
+        case 3:
+            for (i = start; i < end; i++){
+                qry = "UPDATE Schedule SET "+days[3][i-6]+"=? WHERE id_user=?";
+                connection.query(qry,[1, id],  function (error, results, fields) {
+                    if (error) throw error;
+                });
+            }
+            break;
+        case 4:
+            for (i = start; i < end; i++){
+                qry = "UPDATE Schedule SET "+days[4][i-6]+"=? WHERE id_user=?";
+                connection.query(qry,[1, id],  function (error, results, fields) {
+                    if (error) throw error;
+                });
+            }
+            break;
+        case 5:
+            for (i = start; i < end; i++){
+                qry = "UPDATE Schedule SET "+days[5][i-6]+"=? WHERE id_user=?";
+                connection.query(qry,[1, id],  function (error, results, fields) {
+                    if (error) throw error;
+                });
+            }
+            break;
+        case 6:
+            for (i = start; i < end; i++){
+                qry = "UPDATE Schedule SET "+days[6][i-6]+"=? WHERE id_user=?";
+                connection.query(qry,[1, id],  function (error, results, fields) {
+                    if (error) throw error;
+                });
+            }
+            break;
+        default:
+            break;
+    }
+    
 });
--- front/src/store.js
+++ front/src/store.js
@@ -39,7 +39,27 @@ export default window.auth = {
             })
         })
     },
+    deleteEvent:(day1, start1, stop1, callback)=>{
+        const baseUrl ='http://localhost:3001';
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
 
+        fetch(baseUrl + '/deleteSchedule', {
+            method: 'POST',
+            headers,
+            body : JSON.stringify({
+                day: day1,
+                start: start1,
+                end: stop1,
+                id: localStorage.getItem('id')
+            })
+        }).then((response) => {
+            var data = response.json().then((data) => {
+                // console.log(data)
+            callback(data)
+            })
+        })
+    },
     
 
 }
