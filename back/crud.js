e1cab0da702c81fa503546a8f4c96f263707693f
guizg <guizgraicer@gmail.com>
2017-11-27 15:01:39 -0200

    fredao dos mvps

--- back/crud.js
+++ back/crud.js
@@ -4,6 +4,21 @@ var express = require('express');
 var app = express();
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
+var listQuery = [];
+var teamSchedules = [];
+var numberMembers = 0;
+var countQuery = 0;
+var countSchedules = 0;
+
+var days = [["monday_6","monday_7","monday_8","monday_9","monday_10","monday_11","monday_12","monday_13","monday_14","monday_15","monday_16","monday_17","monday_18","monday_19","monday_20","monday_21","monday_22","monday_23"],
+["tuesday_6","tuesday_7","tuesday_8","tuesday_9","tuesday_10","tuesday_11","tuesday_12","tuesday_13","tuesday_14","tuesday_15","tuesday_16","tuesday_17","tuesday_18","tuesday_19","tuesday_20","tuesday_21","tuesday_22","tuesday_23"],
+["wednesday_6","wednesday_7","wednesday_8","wednesday_9","wednesday_10","wednesday_11","wednesday_12","wednesday_13","wednesday_14","wednesday_15","wednesday_16","wednesday_17","wednesday_18","wednesday_19","wednesday_20","wednesday_21","wednesday_22","wednesday_23"],
+["thursday_6","thursday_7","thursday_8","thursday_9","thursday_10","thursday_11","thursday_12","thursday_13","thursday_14","thursday_15","thursday_16","thursday_17","thursday_18","thursday_19","thursday_20","thursday_21","thursday_22","thursday_23"],
+["friday_6","friday_7","friday_8","friday_9","friday_10","friday_11","friday_12","friday_13","friday_14","friday_15","friday_16","friday_17","friday_18","friday_19","friday_20","friday_21","friday_22","friday_23"],
+["saturday_6","saturday_7","saturday_8","saturday_9","saturday_10","saturday_11","saturday_12","saturday_13","saturday_14","saturday_15","saturday_16","saturday_17","saturday_18","saturday_19","saturday_20","saturday_21","saturday_22","saturday_23"],
+["sunday_6","sunday_7","sunday_8","sunday_9","sunday_10","sunday_11","sunday_12","sunday_13","sunday_14","sunday_15","sunday_16","sunday_17","sunday_18","sunday_19","sunday_20","sunday_21","sunday_22","sunday_23"]  
+];
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +33,7 @@ app.listen(3001, function () {
 var connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : '1234',
+    password : '160520',
     database : 'mrmeetings'
     })
 
@@ -140,14 +155,7 @@ app.post('/updateSchedule', function (req, res){
     var action = req.body.action;
 
 
-    days = [["monday_6","monday_7","monday_8","monday_9","monday_10","monday_11","monday_12","monday_13","monday_14","monday_15","monday_16","monday_17","monday_18","monday_19","monday_20","monday_21","monday_22","monday_23"],
-    ["tuesday_6","tuesday_7","tuesday_8","tuesday_9","tuesday_10","tuesday_11","tuesday_12","tuesday_13","tuesday_14","tuesday_15","tuesday_16","tuesday_17","tuesday_18","tuesday_19","tuesday_20","tuesday_21","tuesday_22","tuesday_23"],
-    ["wednesday_6","wednesday_7","wednesday_8","wednesday_9","wednesday_10","wednesday_11","wednesday_12","wednesday_13","wednesday_14","wednesday_15","wednesday_16","wednesday_17","wednesday_18","wednesday_19","wednesday_20","wednesday_21","wednesday_22","wednesday_23"],
-    ["thursday_6","thursday_7","thursday_8","thursday_9","thursday_10","thursday_11","thursday_12","thursday_13","thursday_14","thursday_15","thursday_16","thursday_17","thursday_18","thursday_19","thursday_20","thursday_21","thursday_22","thursday_23"],
-    ["friday_6","friday_7","friday_8","friday_9","friday_10","friday_11","friday_12","friday_13","friday_14","friday_15","friday_16","friday_17","friday_18","friday_19","friday_20","friday_21","friday_22","friday_23"],
-    ["saturday_6","saturday_7","saturday_8","saturday_9","saturday_10","saturday_11","saturday_12","saturday_13","saturday_14","saturday_15","saturday_16","saturday_17","saturday_18","saturday_19","saturday_20","saturday_21","saturday_22","saturday_23"],
-    ["sunday_6","sunday_7","sunday_8","sunday_9","sunday_10","sunday_11","sunday_12","sunday_13","sunday_14","sunday_15","sunday_16","sunday_17","sunday_18","sunday_19","sunday_20","sunday_21","sunday_22","sunday_23"]  
-    ]
+    
 
     switch(day){
         case 0:
@@ -280,65 +288,133 @@ app.post('/findSchedule', function (req, res){
 
 });
 
-function getId(user,callback) {
-    var id = null;
+function getId(user) {
+    var idMembro = null;
+    
     connection.query("SELECT id FROM User WHERE name=?",[user], function (error, results, fields) {
         if (error) callback(error,null); 
         else{//res.json(req.body)
-            var data = JSON.stringify(results);
-            var json = JSON.parse(data);
-            id = json[0]['id'];
-            return this.results;
+            setID(results);
+            
             
         }  
             
     });
+
+    function setID(valor){
+        var data = JSON.stringify(valor);
+        var json = JSON.parse(data);
+        idMembro = json[0]['id'];
+        listQuery.push(idMembro);
+        countQuery++;
+        connection.query("SELECT * FROM Schedule WHERE id_user=?",[idMembro], function(error, results, fields){
+            //console.log(results);
+            var data = JSON.stringify(results);
+            var json = JSON.parse(data);
+            teamSchedules.push(json[0]);
+            countSchedules++;
+            if(countQuery === numberMembers && countSchedules === numberMembers){
+                var qry = makeQueryString();
+                connection.query(qry,listQuery, function (error, results, fields) {
+                    if (error) throw error;   
+                }); 
+                // console.log(teamSchedules);
+                // console.log("moday 6: "+teamSchedules[0]['monday_6']);
+
+                reunion = findSpareTime();
+                
+            }
+        });
+    }
+    //console.log("idMembro fora: "+idMembro);
+    return idMembro;
     
+}
+
+function findSpareTime(){ // retorna o 1o horario livre de todos
+    spareTime = [];
+
+    firstMember = teamSchedules[0];
+
+    for(var i = 0; i<7; i++){
+        for(var j = 0; j<18; j++){
+            if(firstMember[days[i][j]] ===  1){
+                spareTime.push[days[i][j]];
+            }
+        }
+        
+    }
+
+    console.log(spareTime);
+
+    for(var i = 1; i< numberMembers; i++){ // Todos os membros a partir do 2o
+        
+    }
+}
+
+function makeQueryString(){
+
+    var query = "INSERT INTO Team (title, "
+    var query2 = "values(?, ";
+
+    for (var i=1; i<numberMembers+1; i++){
+        if(i == numberMembers){
+            query += ("id_user" + String(i) + ") ");
+            query2 += "?)"
+        }
+        else{
+            query += ("id_user" + String(i) + ", ");
+            query2 += "?, "
+        }  
+    }
+
+    query = query + query2;
+
+    return query;
+
 }
 
 // CRIA GRUPO
 app.post('/registerGroup', function (req, res) {
     //    var data = JSON.parse(req.body);
        //console.log(req);
-    
+
+        countQuery = 0;
+        countSchedules = 0;
+
         var title = req.body.title;
         var members = req.body.members;
-        var numberMembers = req.body.numberMembers;
-        var listQuery =[title];
+        numberMembers = req.body.numberMembers;
+        listQuery =[title];
         var members = JSON.stringify(members);
-        var members = JSON.parse(members)
+        var members = JSON.parse(members);
         
         // var query ="INSERT INTO Team (title, id_user1, id_user2) values(?, ?, ?)"
-        var query = "INSERT INTO Team (title, "
-        var query2 = "values(?, ";
+        
         
         for(var i = 1; i< numberMembers + 1; i++){
-            console.log(getId(members['member' + String(i)],function(err,data){}));
-                
-
-
-            
-            if(i == numberMembers){
-                query += ("id_user" + String(i) + ") ");
-                query2 += "?)"
-            }
-            else{
-                query += ("id_user" + String(i) + ", ");
-                query2 += "?, "
-            }  
+            getId(members['member' + String(i)]);
         }
-        query = query + query2;
-        console.log(query);
-        console.log(listQuery);
+
+
         
+        //console.log(listQuery);
 
  
-        connection.query(query,listQuery, function (error, results, fields) {
-            if (error) throw error;   
-        }); 
-   
-    
-    
-        
     
     });
+
+    // var someVar = [];
+    
+    // connection.query("select * from ROOMS", function(err, rows){
+    //   if(err) {
+    //     throw err;
+    //   } else {
+    //     setValue(rows);
+    //   }
+    // });
+    
+    // function setValue(value) {
+    //   someVar = value;
+    //   console.log(someVar);
+    // }
